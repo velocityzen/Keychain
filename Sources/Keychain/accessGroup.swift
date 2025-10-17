4728c80d11a295e4d3b828a0e69578aea62116cf
Alexey Novikov <v@2dubs.com>
2025-10-17 11:23:17 -0400

    Add macOS conditional compilation for Keychain access group

--- Sources/Keychain/accessGroup.swift
+++ Sources/Keychain/accessGroup.swift
@@ -1,65 +1,67 @@
 import Foundation
 import Security
 
-public func getKeychainAccessGroups() -> Result<[String], KeychainError> {
-    var secCode: SecCode?
-    let status = SecCodeCopySelf([], &secCode)
+#if os(macOS)
+    public func getKeychainAccessGroups() -> Result<[String], KeychainError> {
+        var secCode: SecCode?
+        let status = SecCodeCopySelf([], &secCode)
 
-    guard status == errSecSuccess else {
-        return .failure(.error(status))
-    }
-
-    guard let code = secCode else {
-        return .failure(.noSecCode)
-    }
-
-    var secInfo: CFDictionary?
-    let infoStatus = SecCodeCopySigningInformation(
-        code as! SecStaticCode,
-        SecCSFlags(rawValue: kSecCSSigningInformation),
-        &secInfo
-    )
-
-    guard infoStatus == errSecSuccess else {
-        return .failure(.error(status))
-    }
-
-    guard
-        let info = secInfo as? [String: Any],
-        let entitlements = info[kSecCodeInfoEntitlementsDict as String] as? [String: Any],
-        let groups = entitlements["keychain-access-groups"] as? [String]
-    else {
-        return .failure(.noAccessGroups)
-    }
-
-    return .success(groups)
-}
-
-public func withCurrentAccessGroup() -> (_ attributes: KeychainItemAttributes) -> Result<
-    KeychainItemAttributes, KeychainError
-> {
-    var currentAccessGroup: String?
-
-    func with(_ attributes: KeychainItemAttributes) -> Result<
-        KeychainItemAttributes, KeychainError
-    > {
-        guard let currentAccessGroup else {
-            return getKeychainAccessGroups()
-                .flatMap { groups -> Result<String, KeychainError> in
-                    guard let accessGroup = groups.first else {
-                        return .failure(.noAccessGroups)
-                    }
-
-                    return .success(accessGroup)
-                }
-                .map { accessGroup -> KeychainItemAttributes in
-                    currentAccessGroup = accessGroup
-                    return withAccessGroup(accessGroup)(attributes)
-                }
+        guard status == errSecSuccess else {
+            return .failure(.error(status))
         }
 
-        return .success(withAccessGroup(currentAccessGroup)(attributes))
+        guard let code = secCode else {
+            return .failure(.noSecCode)
+        }
+
+        var secInfo: CFDictionary?
+        let infoStatus = SecCodeCopySigningInformation(
+            code as! SecStaticCode,
+            SecCSFlags(rawValue: kSecCSSigningInformation),
+            &secInfo
+        )
+
+        guard infoStatus == errSecSuccess else {
+            return .failure(.error(status))
+        }
+
+        guard
+            let info = secInfo as? [String: Any],
+            let entitlements = info[kSecCodeInfoEntitlementsDict as String] as? [String: Any],
+            let groups = entitlements["keychain-access-groups"] as? [String]
+        else {
+            return .failure(.noAccessGroups)
+        }
+
+        return .success(groups)
     }
 
-    return with
-}
+    public func withCurrentAccessGroup() -> (_ attributes: KeychainItemAttributes) -> Result<
+        KeychainItemAttributes, KeychainError
+    > {
+        var currentAccessGroup: String?
+
+        func with(_ attributes: KeychainItemAttributes) -> Result<
+            KeychainItemAttributes, KeychainError
+        > {
+            guard let currentAccessGroup else {
+                return getKeychainAccessGroups()
+                    .flatMap { groups -> Result<String, KeychainError> in
+                        guard let accessGroup = groups.first else {
+                            return .failure(.noAccessGroups)
+                        }
+
+                        return .success(accessGroup)
+                    }
+                    .map { accessGroup -> KeychainItemAttributes in
+                        currentAccessGroup = accessGroup
+                        return withAccessGroup(accessGroup)(attributes)
+                    }
+            }
+
+            return .success(withAccessGroup(currentAccessGroup)(attributes))
+        }
+
+        return with
+    }
+#endif
